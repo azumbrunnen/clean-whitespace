b9a56dcbb048e147811e336c70bf00ed8d805643
Adrian Zumbrunnen <azumbrunnen@gmail.com>
2015-04-15 15:02:55 +0200

    initial

---
+++ index.js
@@ -0,0 +1,16 @@
+function cleanWhitespace = function( node ){
+	for(var n = 0; n < node.childNodes.length; n++){
+        var child = node.childNodes[n];
+
+        if ( child.nodeType === 8 || ( child.nodeType === 3 && !/\S/.test( child.nodeValue ) ) ) {
+            node.removeChild( child );
+            n--;
+        } else if ( child.nodeType === 1) {
+            cleanWhitespace( child );
+        } else if ( child.nodeType === 3 && /(^\s+|\s+$)/.test( child.parentNode.innerHTML ) ){
+            child.parentNode.innerHTML = child.parentNode.innerHTML.replace(/(^\s+|\s+$)/g, '');
+        }
+	}
+};
+
+module.exports = cleanWhitespace;
